f2d30b6325fefd837a49e2cca47a7d5886b53150
montanamott <35900138+montanamott@users.noreply.github.com>
2018-05-18 15:38:17 -0400

    Added initial skeletization of functions

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<cmath>
 
+//REQUIRES: aVal, bVal, and eVal must all be positive integers
+//EFFECTS: Returns the difference between the C value 
+// (A^E + B^E)^(1/E) and its nearest integer
+double difference(int aVal, int bVal, int eVal)
+{
+	return 0;
+}
+
+//Will loop through different exponents and integers 
+//to determine which A^E + B^E = C^E comes closest 
+//to satisfying Fermat's Last Theorem
+int main(int argc, char* argv[])
+{
+	return 0;
+}
